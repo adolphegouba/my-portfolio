0ebc085fe5115bfaf60ce5c2f62ba771677af82d
adolphegouba <adolphegouba4@gmail.com>
2025-11-30 16:50:15 +0000

    ajout balise analytique

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Providers } from '@/components/providers';
 import { SiteHeader } from '@/components/layout/site-header';
@@ -61,6 +62,7 @@ export default async function RootLayout({
             </Providers>
           </GlobalDataProvider>
         </div>
+        <Analytics />
       </body>
     </html>
   );
